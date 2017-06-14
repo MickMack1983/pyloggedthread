225f9557b9c9064d43d09c4bd58a899981e3556d
Mikael Magnusson <mikael.magnusson@cybercom.com>
2017-06-14 16:18:17 +0200

    added ErrorLoggedThread

--- pyloggedthread/loggedthread.py
+++ pyloggedthread/loggedthread.py
@@ -28,3 +28,24 @@ class LoggedThread:
             if not self.log:
                 self.log = logging.getLogger("LoggedThread-" + str(self.t.ident))
             self.log.debug("terminated")
+
+
+class ErrorLoggedThread:
+    def __init__(self, group=None, target=None, name=None, log=None, *args, **kwargs):
+        self.__target = target
+        self.log = log if log else None
+        self.t = Thread(group=group, target=self.wrapper, name=name, *args, **kwargs)
+
+    def start(self):
+        self.t.start()
+
+    def wrapper(self, *args, **kwargs):
+        # noinspection PyBroadException
+        try:
+            self.__target(*args, **kwargs)
+        except Exception as e:
+            if not self.log:
+                self.log = logging.getLogger("LoggedThread-" + str(self.t.ident))
+            self.log.error(traceback.format_exc())
+            raise e
+
